304bb7928f72779e62cb2c46ac47ed35acf63b03
UNTORO <37415183+untoro-id@users.noreply.github.com>
2023-09-22 17:01:53 +0700

    update js untuk menu toggle
    Signed-off-by: UNTORO <37415183+untoro-id@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -15,3 +15,13 @@ function showSlide() {
     setTimeout(showSlide, 3000);
 
 }
+
+//Menu Icon
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav-menu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+
+}
